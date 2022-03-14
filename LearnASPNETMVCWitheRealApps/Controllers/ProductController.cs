6d5772e94603ded4ddeccb42477b0a84adc67ffe
NingZhang <selenazhang@outlook.com>
2022-03-13 22:24:40 -0700

    Add LearnASPNETMVCWithRealApps

---
+++ LearnASPNETMVCWitheRealApps/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using LearnASPNETMVCWitheRealApps.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnASPNETMVCWitheRealApps.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            ProductModel productModel = new ProductModel();
+            ViewBag.products = productModel.FindAll();
+            return View();
+        }
+    }
+}
---
+++ LearnASPNETMVCWitheRealApps/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LearnASPNETMVCWitheRealApps")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LearnASPNETMVCWitheRealApps")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dc391cb7-cc3c-4279-8225-8a945eb3b9b0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
